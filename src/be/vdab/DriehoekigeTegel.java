e4b971f723e6beb4172b19d9ce88317d8fe08826
michelle422 <michelle.norico@vdabcampus.be>
2018-01-23 09:59:09 +0100

    definitieve oplossing

--- src/be/vdab/DriehoekigeTegel.java
+++ src/be/vdab/DriehoekigeTegel.java
@@ -2,11 +2,11 @@ package be.vdab;
 
 import java.math.BigDecimal;
 
-public class DriekhoekigeTegel extends Tegel{
+public class DriehoekigeTegel extends Tegel{
     private final double lengte;
     private final double breedte;
 
-    public DriekhoekigeTegel(BigDecimal prijs, double lengte, double breedte) {
+    public DriehoekigeTegel(BigDecimal prijs, double lengte, double breedte) {
         super(prijs);
         this.lengte = lengte;
         this.breedte = breedte;
--- src/be/vdab/Main.java
+++ src/be/vdab/Main.java
@@ -5,7 +5,15 @@ import java.math.BigDecimal;
 public class Main {
 
     public static void main(String[] args) {
-        Tegel tegel1 = new VierkanteTegel(new BigDecimal(3), 30.0);
+        Tegel tegel1 = new VierkanteTegel(new BigDecimal(3),30.0);
+        Tegel tegel2 = new RechthoekigeTegel(new BigDecimal(5),40.0,20.0);
+        Tegel tegel3 = new DriehoekigeTegel(new BigDecimal(4),40.0,15);
+        Bestelling bestelling = new Bestelling();
+        bestelling.detailToevoegen(tegel1,10);
+        bestelling.detailToevoegen(tegel2,3);
+        bestelling.detailToevoegen(tegel3,15);
+        bestelling.WijzigAantalVanDetail(tegel2,5);
+        System.out.println(bestelling.getOppervlakte());
+        System.out.println(bestelling.getTeBetalen());
     }
-    
 }
